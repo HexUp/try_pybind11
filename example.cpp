cc4cd4185b737ce6c5df607bc26018566165b268
HexUp <47908719+HexUp@users.noreply.github.com>
2024-03-02 12:02:15 +0800

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,12 @@
+#include <pybind11/pybind11.h>
+
+int add(int i, int j) {
+    return i + j;
+}
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(example, m) {
+    m.doc() = "pybind11 example plugin";
+    m.def("add", &add, "A function which adds two numbers");
+}
